9e87d0e03aba95ada57f9721a0a93656febc5932
Arman Arif <arman3472rmn@gmail.com>
2021-07-20 15:55:31 +0600

    python string operations

---
+++ 7-string_operation.py
@@ -0,0 +1,18 @@
+# String Operations
+
+print('#' * 20)
+
+text = "Python is Awesome"
+
+print(len(text))
+print(text.upper())
+print(text.lower())
+print(text)
+print(text.title())
+print(text.find('A'))
+print(text.find('is'))
+print(text.replace('Awesome', 'very Awesome'))
+
+print('Python' in text)
+print('python' in text)
+
