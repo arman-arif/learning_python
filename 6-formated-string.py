f751789a031738cb9a05404bbde0e9d33c6074a8
Arman Arif <arman3472rmn@gmail.com>
2021-07-20 15:55:13 +0600

    python string formating

---
+++ 6-formated-string.py
@@ -0,0 +1,11 @@
+first_name = 'Arman'
+last_name = 'Arif'
+
+normal_msg = first_name + ' [' + last_name + '] is a coder.'
+
+print('Normal Message : ' + normal_msg)
+
+formatted_msg = f'{first_name} [{last_name}] is a coder.'
+
+print('Formatted Message : ' + formatted_msg)
+
