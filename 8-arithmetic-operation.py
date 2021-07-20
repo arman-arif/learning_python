e9e59bd9826a6cb3f3f73b23773d5cb886a75e09
Arman Arif <arman3472rmn@gmail.com>
2021-07-20 15:55:56 +0600

    python arithmetic operations

---
+++ 8-arithmetic-operation.py
@@ -0,0 +1,28 @@
+# Arithmetic Operations
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)  # returns integer
+print(10 % 3)   # returns reminder
+print(10 ** 3)  # returns power
+
+x = 10
+# augmented / enhance assigment operator
+x += 3  # x = x + 3
+print(x)
+
+x -= 7  # x = x - 7
+print(x)
+
+# presidency
+# -> bracket / parenthesis
+# -> exponential
+# -> division or multiplication
+# -> addition or subtraction
+x = 10 + 3 * 2 ** 2
+print(x)
+x = (2+3) * 10 - 3
+print(x)
+
