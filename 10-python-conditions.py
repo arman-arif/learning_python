76cd018fa2129c2592fcce22bf970e2e5bdff4a3
Arman Arif <arman3472rmn@gmail.com>
2021-07-21 00:38:52 +0600

    python conditions with example

--- 10-python-conditions.py
+++ 10-python-conditions.py
@@ -1,8 +1,24 @@
-is_hot = True
+# is_hot = True
+# is_cold = False
 
-if is_hot:
-    print("It's a hot day.")
-    print("Drink a plenty of water.")
-elif not is_hot:
-    print("It's a cold day.")
-print("Enjoy your day!")
+# if is_hot:
+#     print("It's a hot day.")
+#     print("Drink a plenty of water.")
+
+# elif is_cold:  # elif not is_hot:
+#     print("It's a cold day.")
+#     print('Ware warm cloths.')
+# else: 
+#     print("It's a lovely day.")
+# print("Enjoy your day!")
+
+# Example
+landPrice = 1000000
+hasGoodCredit = False
+
+if hasGoodCredit:
+    downPayment = landPrice * 0.1
+else:
+    downPayment = landPrice * 0.2
+
+print(f'Your down payment: ${downPayment}')
