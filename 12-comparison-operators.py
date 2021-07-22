f9b387bd9f244b1f85f761e64869128e27eaf168
Arman Arif <arman3472rmn@gmail.com>
2021-07-22 13:06:25 +0600

    Python comparison operators

---
+++ 12-comparison-operators.py
@@ -0,0 +1,21 @@
+# Comparison Operators
+# == < > <= >= !=
+
+temparature = 37
+
+if temparature > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+
+# Example
+name = "Jr."
+
+if len(name) < 3:
+    print("Name is to short")
+elif len(name) > 50:
+    print("Invalid name given")
+else:
+    print("Name looks good!")
+
